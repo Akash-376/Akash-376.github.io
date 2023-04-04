d264679b851cf1bfa0c7f0f2399b897eeb33722b
Akash Chauhan <112763866+Akash-376@users.noreply.github.com>
2023-04-04 19:29:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 // })
 
 
-// function resume() {
+function resume() {
     // var win = window.open('Akash_Chauhan_Resume.pdf', '_blank');
-    // var win = window.open("https://drive.google.com/file/d/1aXEimS1Tyv45SANM9-KKgpjXxlmEOf-d/view?usp=share_link", "_blank");
-    // win.focus();
-// }
+    var win = window.open("https://drive.google.com/file/d/1aXEimS1Tyv45SANM9-KKgpjXxlmEOf-d/view?usp=share_link", "_blank");
+    win.focus();
+}
 
 
 var navbar = document.getElementById('nav-menu');
@@ -41,36 +41,3 @@ function activeMenu(){
 activeMenu();
 window.addEventListener("scroll", activeMenu);
 
-
-
-// new code
-let menu=document.getElementById("menu-icon");
-let navbar=document.querySelector(".navbar");
-let m=document.querySelector(".main-body")
-let count=0;
-menu.addEventListener('click',()=>{
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-    
-    m.style.marginTop="200px";
-    count++;
-    if(count%2==0){
-        m.style.marginTop="0px";
-
-    }
-
-    if(m.classList.contains("m-t")){
-        m.classList.remove("m-t");
-    }
-    else{
-        m.classList.add("m-t");
-    }
-});
-
-document.getElementById("resume-button-1").onclick = ()=>{
-    window.open("https://drive.google.com/file/d/1aXEimS1Tyv45SANM9-KKgpjXxlmEOf-d/view",`_blank`)
-  }
-  
-  document.getElementById("resume-button-2").onclick = ()=>{
-    window.open("https://drive.google.com/file/d/1aXEimS1Tyv45SANM9-KKgpjXxlmEOf-d/view",`_blank`)
-  }
